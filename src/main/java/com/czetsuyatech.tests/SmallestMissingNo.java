b084b5596d51c61c17bf442d146951d1ee90af3b
Edward P. Legaspi <czetsuya@gmail.com>
2023-03-02 14:56:27 +0800

    feat: finding smallest missing no

---
+++ src/main/java/com/czetsuyatech.tests/SmallestMissingNo.java
@@ -0,0 +1,26 @@
+package com.czetsuyatech.tests;
+
+import java.util.BitSet;
+
+public class SmallestMissingNo {
+
+  public static void main(String[] args) {
+
+//    int[] input = {1, 3, 6, 4, 1, 2};
+    int[] input = {98, 85, 91, 96, 100};
+    int output = findSmallestMissingNo(input);
+    System.out.println("result=" + output);
+  }
+
+  public static int findSmallestMissingNo(int[] input) {
+
+    BitSet bs = new BitSet();
+    for (int e : input) {
+      if (e > 0) {
+        bs.set(e);
+      }
+    }
+
+    return bs.nextClearBit(1);
+  }
+}
